fbd3a2d524184a84c0fd3e1ae14a79bc3642018b
shashi <12sk678@gmail.com>
2026-02-17 11:14:36 +0000

    17 feb 2026

--- playground.cpp
+++ playground.cpp
@@ -1,27 +1,72 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
+class Node
+{
+    public:
+        int data;
+        Node* next;
+        Node(int val){
+            data = val;
+            next = NULL;
+        }
+};
+
+class SinglyLinkedList
+{
+    private:
+        Node* head; // head itself is not a node, but just a node pointer that points to the first node
+    public:
+        void Dislpay(){
+            if (head == NULL){
+                cout << "The list is empty\n";
+                return;
+            }
+            Node* temp = head;
+            while(temp->next != NULL)
+            {
+                cout << temp->data << " ->";
+                temp = temp->next;
+            }
+            cout << "Null\n";
+        }
+        void InsertAtBeginning(int val){
+            Node* newNode = new Node(val);
+            newNode->next = head;
+            head = newNode;
+            cout << "Inserted at the beginning\n";
+        }
+        void InsertAtEnd(int val){
+            //check if list is already empty then call InsertAtBeginning(val)
+            Node* newNode = new Node(val);
+            Node* temp = head;
+            while(temp->next!=NULL){
+                temp = temp->next;
+            }
+            temp->next = newNode;
+            cout << "Inserted at the end\n";
+        }
+        void Insert(int val, int pos){
+            if (pos == 0){
+                InsertAtBeginning(val);
+                return;
+            }
+            Node* newNode = new Node(val);
+            Node* temp = head;
+            int i = 0;
+            while (i<pos && temp->next!=NULL){
+                temp = temp->next;
+                i++;
+            }
+            newNode->next = temp->next;
+            temp->next = newNode;
+            cout << "Inserted at " << pos << "th position\n";
+        }
+        
+};
+
 int main(){
-    int n, pos;
-    cout << "Enter n: ";
-    cin >> n;
-    int arr[n];
-    cout << "\nEnter n elements: ";
-    for (int i=0;i<n;i++){
-        cin >> arr[i];
-    }
-    cout << "\n----------\n";
-    cout << "Type 0 for bubble sort and 1 for insertion sort: ";
-    int choice;
-    cin >> choice;
-    cout << "\n";
-    switch (choice){
-        case 0:
-            bubble(arr, n);
-            break;
-        case 1:
-            insertion(arr, n);
-            break;
-    }
+
     return 0;
 }
